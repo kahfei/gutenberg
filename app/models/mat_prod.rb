3a68169fd8c8367f318a0db843c4043b56f08c8b
kahfei <bak.kahfei@gmail.com>
2014-08-25 00:06:37 +0800

    validation

--- app/models/mat_prod.rb
+++ app/models/mat_prod.rb
@@ -1,2 +1,19 @@
 class MatProd < ActiveRecord::Base
+	validates :matprod_category_code, presence: true
+    validates :matprod_sub_code, presence: true
+    validates :matprod_type, presence: true
+    validates :matprod_code, presence: true
+    validates :matprod_shortdesc, presence: true
+    validates :matprod_remarks, presence: true
+    validates :matprod_status, presence: true
+    validates :matprod_cost_price, presence: true
+    validates :matprod_selling_price, presence: true
+    validates :matprod_uom, presence: true
+    validates :matprod_photo, presence: true
+    validates :matprod_cost_code, presence: true
+    validates :updated_user_id, presence: true
+    validates :matprod_est_weight_gram, presence: true
+    validates :matprod_size_mm, presence: true
+    validates :matprod_length_mm, presence: true
+    validates :matprod_thikness_mm, presence: true
 end
